c9a124ccd0ce4e26f9e89ec413f4182dbecfec39
Patricio Ibar <patricioibar10@gmail.com>
2024-05-29 11:06:25 -0300

    agregado buscarZorro
    testeado en un archivo aparte

--- funcionalidad.asm
+++ funcionalidad.asm
@@ -1,7 +1,17 @@
 global copiarTablero
 
 section .data
+    ; como se representan diferentes elementos en el tablero
+    repZorro                db 2
+    repOcas                 db 1
+    repEspacio              db 0
+
+    longitudFila            dq 7
+    longitudElemento        dq 1
 section .bss
+    iteradorFila            resq 1
+    iteradorCol             resq 1
+
 section .text
 
 ; copia los 49 bytes comenzando por la direccion en rsi (source)
@@ -16,4 +26,47 @@ copiarTableroBucle:
     inc     rcx
     jmp     copiarTableroBucle
 copiarTableroFin:
+    ret
+
+; busca la posición del zorro en el tablero que incia en la direccion rdi
+; devuelve la fila en rax y la columna en rbx
+; devuelve valores para usar directamente en código, es decir números entre 0 y 6
+; si no se encuentra el zorro, devuelve -1 en rax (no debería pasar nunca)
+buscarZorro:
+    mov     qword[iteradorFila],0
+    mov     qword[iteradorCol],0
+buscarZorroBucle:
+    cmp     qword[iteradorCol],7
+    jge     buscarZorroProximaFila
+
+    mov     rax,qword[longitudElemento]
+    imul    rax,qword[iteradorCol]
+
+    mov     rbx,qword[longitudFila]
+    imul    rbx,qword[iteradorFila]
+
+    add     rax,rbx
+    add     rax,rdi
+
+    mov     bl,byte[rax]
+    cmp     bl,[repZorro]
+    je      buscarZorroEncontrado
+
+    inc     qword[iteradorCol]
+    jmp     buscarZorroBucle
+
+buscarZorroProximaFila:
+    cmp     qword[iteradorFila],7
+    jge     buscarZorroNoEncontrado
+    inc     qword[iteradorFila]
+    mov     qword[iteradorCol],0
+    jmp     buscarZorroBucle
+
+buscarZorroNoEncontrado:
+    mov     rax,-1
+    ret
+    
+buscarZorroEncontrado:
+    mov     rbx,[iteradorCol]
+    mov     rax,[iteradorFila]
     ret
